c52be9626f4b859948c42e219d435c4c4a53ce3c
Taco10101110 <tyocarnivore@gmail.com>
2022-02-06 22:02:08 +0900

    For Bussines card

--- pages/p/designpattern/index.vue
+++ pages/p/designpattern/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <TclTexture>
-    <h2>Webデザインパターン</h2>
+  <TclTexture class="background" :pattern="bgpattern" :color1="bgcolor1" :color2="bgcolor2" :color3="bgcolor3">
+    <TclPanel class="panel" :pattern="'grass-01'">
+      <h1 class="title">
+        <TclTypography
+          class="title-typo"
+          :pattern="'retro-02'"
+          :content="'工事中です'"
+          :font="'Rampart One'"
+        />
+      </h1>
+    </TclPanel>
   </TclTexture>
 </template>
 <script>
@@ -9,7 +18,13 @@ export default {
   components: {
     TclTexture
   },
-  layout: 'default'
+  layout: 'default',
+  data: () => ({
+    bgpattern: 'check-01',
+    bgcolor1: '#F0F0F0',
+    bgcolor2: '#0B2342',
+    bgcolor3: '#51a9df'
+  })
 }
 </script>
 <style scoped lang="scss">
